3523764aac590b9604519beaba4b72a0cbe81798
Angela L <laveryangela@gmail.com>
2024-12-07 18:25:34 +0000

    adding initial tests

---
+++ Templates/python_syntax_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestPythonSyntax(unittest.TestCase):
+    def test_syntax(self):
+        try:
+            with open('/dbfs/FileStore/pricing_123.py', 'r') as file:  # Adjust the path as needed
+                code = file.read()
+            compile(code, 'pricing_123.py', 'exec')
+        except SyntaxError as e:
+            self.fail(f"Syntax error in file: {e}")
+
+class CustomTestRunner(unittest.TextTestRunner):
+    def run(self, test):
+        result = super().run(test)
+        if result.wasSuccessful():
+            print("\nAll tests passed successfully!")
+        return result
+
+if __name__ == '__main__':
+    unittest.main(testRunner=CustomTestRunner(verbosity=0))
